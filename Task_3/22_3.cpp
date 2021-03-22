028bca59223e7ed00c8569c23d531411fea242f6
korshunov80@mail.ru <korshunov80@mail.ru>
2021-03-22 10:32:56 +0300

    Update 22_3.cpp

--- Task_3/22_3.cpp
+++ Task_3/22_3.cpp
@@ -5,25 +5,34 @@
 int main() {
     std::string name;
     std::string family;
-    double sum;
-    std::string paymentDate;
-    std::fstream bill;
+    double sum = 0;
     double maxSum = 0;
     std::string maxName;
     std::string maxFamily;
     double total = 0;
-    bill.open("C:\\Users\\User\\CLionProjects\\22.3\\bill.txt");
+    std::string paymentDate;
+
+    std::ifstream bill;
+
+    bill.open("..\\bill.txt");
+
+    if (!bill.is_open()) {
+        std::cerr << "File is not open!!!" << std::endl;
+        return 1;
+    }
 
     while (!bill.eof()) {
-        bill >>  name >> family >> sum >> paymentDate;
-        total += sum;
-        if (maxSum < sum) {
+            bill >> name >> family >> sum >> paymentDate;
+            total += sum;
+            if (maxSum < sum) {
                 maxSum = sum;
                 maxName = name;
                 maxFamily = family;
-        }
+            }
     }
+
     bill.close();
+
     std::cout << "Total: " << total << std::endl;
     std::cout << "Maximum: " << maxName << " " << maxFamily << " " << maxSum;
 
