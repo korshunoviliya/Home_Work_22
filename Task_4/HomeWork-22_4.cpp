43885007f7a6da90c0031f11e9a95abe74fad15b
korshunov80@mail.ru <korshunov80@mail.ru>
2021-03-22 10:33:04 +0300

    Update HomeWork-22_4.cpp

--- Task_4/HomeWork-22_4.cpp
+++ Task_4/HomeWork-22_4.cpp
@@ -7,19 +7,23 @@ int main() {
     std::string pathFile;
     std::cout << "Enter the absolute path to the text file: ";
     std::cin >> pathFile;
+    std::string fileExtension = pathFile.substr(pathFile.length() - 3);
+    if (fileExtension != "png") {
+        std::cerr << "File is NOT format PNG\n";
+        return 0;
+    }
     char buffer[4];
-
     picture.open(pathFile, std::ios::binary);
 
     if (picture.is_open()) {
          picture.read(buffer, 4);
          if (buffer[0] = -119 && buffer[1] == 'P' && buffer[2] == 'N' && buffer[3] == 'G') {
-             std::cout << "File is format PNG\n";
+             std::cerr << "File is format PNG\n";
          }
-         else std::cout << "File is NOT format PNG\n";
+         else std::cerr << "File is NOT format PNG\n";
     }
-    else std::cout << "File is not open!!!" << std::endl;
+    else std::cerr << "File is not open!!!" << std::endl;
 
     picture.close();
-
+return 0;    
 }
